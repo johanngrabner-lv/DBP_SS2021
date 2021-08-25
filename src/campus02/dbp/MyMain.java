bf57693d6def46ce3410e83df1b29ca165d5dbe1
johanngrabner-lv <johann.grabner@lv.campus02.at>
2021-08-25 13:51:45 +0200

    Create New Database

--- src/campus02/dbp/MyMain.java
+++ src/campus02/dbp/MyMain.java
@@ -5,15 +5,10 @@ import java.sql.*;
 public class MyMain {
     public static void main(String[] args){
 
-        Person p=new Person(); //Objekt instanziieren
-
-        //Class.forName("campus02.dbp.Person");
-
 
         System.out.print("Hello Campus02");
-       // createNewDatabase("t1.db");
-      //  connect("t1.db");
-        createNewTable("t1.db");
+        createNewDatabase("t1.db");
+
     }
 
     public static void createNewDatabase(String fileName) {
@@ -32,47 +27,4 @@ public class MyMain {
         }
     }
 
-    public static void connect(String fileName) {
-        Connection conn = null;
-        try {
-            // db parameters
-            String url = "jdbc:sqlite:C:/sqlite/db/" + fileName;
-            // create a connection to the database
-            conn = DriverManager.getConnection(url);
-
-            System.out.println("Connection to SQLite has been established.");
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
-    }
-    public static void createNewTable(String fileName) {
-        // SQLite connection string
-        String url = "jdbc:sqlite:C://sqlite/db/" + fileName;
-
-        // SQL statement for creating a new table
-        String sql = "CREATE TABLE IF NOT EXISTS warehouses (\n"
-                + "	id integer PRIMARY KEY,\n"
-                + "	name text NOT NULL,\n"
-                + "	capacity real\n"
-                + ");";
-
-        try (Connection conn = DriverManager.getConnection(url);
-             Statement stmt = conn.createStatement()) {
-            // create a new table
-            stmt.execute(sql);
-            System.out.println("Created");
-        } catch (SQLException e) {
-            System.out.println("Failed");
-            System.out.println(e.getMessage());
-        }
-    }
 }
