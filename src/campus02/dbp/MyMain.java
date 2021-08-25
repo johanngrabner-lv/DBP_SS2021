f8f8f39180dc4d4061ff55dffdf79db09fdc6706
johanngrabner-lv <johann.grabner@lv.campus02.at>
2021-08-25 13:32:29 +0200

    Diverse SQL-Statements

--- src/campus02/dbp/MyMain.java
+++ src/campus02/dbp/MyMain.java
@@ -1,7 +1,72 @@
 package campus02.dbp;
 
+import java.sql.*;
+
 public class MyMain {
     public static void main(String[] args){
         System.out.print("Hello Campus02");
+       // createNewDatabase("t1.db");
+      //  connect("t1.db");
+        createNewTable("t1.db");
+    }
+
+    public static void createNewDatabase(String fileName) {
+
+        String url = "jdbc:sqlite:C:/sqlite/db/" + fileName;
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void connect(String fileName) {
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:C:/sqlite/db/" + fileName;
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    public static void createNewTable(String fileName) {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C://sqlite/db/" + fileName;
+
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS warehouses (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	name text NOT NULL,\n"
+                + "	capacity real\n"
+                + ");";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+            System.out.println("Created");
+        } catch (SQLException e) {
+            System.out.println("Failed");
+            System.out.println(e.getMessage());
+        }
     }
 }
