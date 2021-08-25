1704902c2bd51158495a40b21d0a7c9e60558171
johanngrabner-lv <johann.grabner@lv.campus02.at>
2021-08-25 14:15:12 +0200

    Connect to Database

--- src/campus02/dbp/MyMain.java
+++ src/campus02/dbp/MyMain.java
@@ -4,17 +4,19 @@ import java.sql.*;
 
 public class MyMain {
     public static void main(String[] args){
-
-
         System.out.print("Hello Campus02");
         createNewDatabase("t1.db");
+        connect("t1.db");
 
     }
 
     public static void createNewDatabase(String fileName) {
 
+        //1. Create Folder c:/sqlite/db"
         String url = "jdbc:sqlite:C:/sqlite/db/" + fileName;
 
+        //FILE - Project Structure
+        //Libraries "+" -- sqlite-jdbc-3.30.1.jar
         try (Connection conn = DriverManager.getConnection(url)) {
             if (conn != null) {
                 DatabaseMetaData meta = conn.getMetaData();
@@ -24,6 +26,29 @@ public class MyMain {
 
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    public static void connect(String filename) {
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:C:/sqlite/db/" + filename;
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
         }
     }
 
