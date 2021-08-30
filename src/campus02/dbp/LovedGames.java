c68dd1475a0c53e72a2a2dd430a702d3b91780c1
johanngrabner-lv <johann.grabner@lv.campus02.at>
2021-08-30 09:16:01 +0200

    getMyLovedGames

--- src/campus02/dbp/LovedGames.java
+++ src/campus02/dbp/LovedGames.java
@@ -1,4 +1,43 @@
 package campus02.dbp;
 
+import java.util.Date;
+
 public class LovedGames {
+    public int getLovedGamesId() {
+        return LovedGamesId;
+    }
+
+    public void setLovedGamesId(int lovedGamesId) {
+        LovedGamesId = lovedGamesId;
+    }
+
+    public int getPlayerId() {
+        return PlayerId;
+    }
+
+    public void setPlayerId(int playerId) {
+        PlayerId = playerId;
+    }
+
+    public int getGameId() {
+        return GameId;
+    }
+
+    public void setGameId(int gameId) {
+        GameId = gameId;
+    }
+
+    public Date getPlayDate() {
+        return PlayDate;
+    }
+
+    public void setPlayDate(Date playDate) {
+        PlayDate = playDate;
+    }
+
+    private int LovedGamesId;
+    private int PlayerId;
+    private int GameId;
+    private Date PlayDate;
+
 }
--- src/campus02/dbp/Player.java
+++ src/campus02/dbp/Player.java
@@ -1,7 +1,11 @@
 package campus02.dbp;
 
+import java.sql.*;
+import java.util.ArrayList;
+
 public class Player {
 
+    private String url = "jdbc:sqlite:C://sqlite/db/donnerstag.db";
     public Player(){
 
     }
@@ -62,6 +66,32 @@ public class Player {
     ArrayList<LovedGames> myLovedGames;
 
     public ArrayList<LovedGames> getMyLovedGames() {
+        if (myLovedGames==null)
+            myLovedGames =new ArrayList<LovedGames>();
+        else
+            myLovedGames.clear();
+
+        String sqlSelect = "SELECT * FROM LovedGames WHERE PlayerId = ?";
+        try (Connection conn = DriverManager.getConnection(url);
+             PreparedStatement preparedStatement = conn.prepareStatement(sqlSelect)) {
+            preparedStatement.setInt(1, PlayerId);
+
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()) {
+                LovedGames g =new LovedGames();
+                g.setLovedGamesId(rs.getInt("LovedGamesId"));
+                g.setGameId(rs.getInt("GameId"));
+                g.setPlayerId(rs.getInt("PlayerId"));
+                g.setPlayDate(rs.getDate("PlayDate"));
+
+                myLovedGames.add(g);
+
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+
         return myLovedGames;
     }
 
